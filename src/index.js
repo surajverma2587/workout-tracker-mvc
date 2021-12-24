d7bfb04402e121751168fcb6bc83f45f7c88915c
surajverma2587 <surajverma2587@gmail.com>
2021-12-24 17:38:14 +0000

    add express server with handlebars

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("hello from the application");
+const express = require("express");
+const expressHandlebars = require("express-handlebars");
+const path = require("path");
+
+const routes = require("./routes");
+
+const PORT = process.env.PORT || 3000;
+
+const hbs = expressHandlebars.create({});
+const app = express();
+
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Navigate to http://localhost:${PORT}`));
