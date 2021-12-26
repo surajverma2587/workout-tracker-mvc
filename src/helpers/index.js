ee29b73cbd2277895eca27fbd9d2ae75e0dfe5e7
surajverma2587 <surajverma2587@gmail.com>
2021-12-26 16:19:05 +0000

    add sign-up POST route to create user

---
+++ src/helpers/index.js
@@ -0,0 +1,10 @@
+const getPayloadWithValidFieldsOnly = (validFields, payload) =>
+  Object.entries(payload).reduce(
+    (acc, [key, value]) =>
+      validFields.includes(key) ? { ...acc, [key]: value } : acc,
+    {}
+  );
+
+module.exports = {
+  getPayloadWithValidFieldsOnly,
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,11 @@
 const { Router } = require("express");
 
+const auth = require("./auth");
 const view = require("./view");
 
 const router = Router();
 
+router.use("/auth", auth);
 router.use("/", view);
 
 module.exports = router;
