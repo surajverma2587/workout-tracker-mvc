4d01cb3e6280f473ba0625f1a965b0b08a1d616a
surajverma2587 <surajverma2587@gmail.com>
2021-12-26 16:02:09 +0000

    connect sequelize to express

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
+require("dotenv").config();
 const express = require("express");
 const expressHandlebars = require("express-handlebars");
 const path = require("path");
 
 const routes = require("./routes");
+const connection = require("./config/connection");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,4 +19,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(routes);
 
-app.listen(PORT, () => console.log(`Navigate to http://localhost:${PORT}`));
+const init = async () => {
+  try {
+    await connection.sync({ force: false });
+
+    console.log("[INFO]: DB connection successful");
+
+    app.listen(PORT, () => console.log(`Navigate to http://localhost:${PORT}`));
+  } catch (error) {
+    console.log(`[ERROR]: DB connection failed | ${error.message}`);
+  }
+};
+
+init();
